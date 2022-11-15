1e88719e8ca8881f6d9639bd15b4047cebb3191a
Hippolyte Handwerk <hippolyte.handwerk@gmail.com>
2022-11-15 17:21:15 +0100

    le clan des Yakuzas

--- src/personnages/Yakuza.java
+++ src/personnages/Yakuza.java
@@ -9,10 +9,10 @@ public class Yakuza extends Humain{
 		this.clan=clan;	
 	}
 	public void extorquer(Commercant victime) {
-		parler("Tiens, tiens, ne serait-ce pas un faible marchand qui passe par là ?");
-		parler(victime.getNom()+", si tu tiens à la vie donne moi ta bourse !");
+		parler("Tiens, tiens, ne serait-ce pas un faible marchand qui passe par lï¿½ ?");
+		parler(victime.getNom()+", si tu tiens ï¿½ la vie donne moi ta bourse !");
 		int butin=victime.seFaireExtorquer();
-		parler("J’ai piqué les "+butin+" sous de "+victime.getNom()+", ce qui me fait "+getArgent()+" sous dans ma poche. Hi ! Hi !");
+		parler("Jï¿½ai piquï¿½ les "+butin+" sous de "+victime.getNom()+", ce qui me fait "+getArgent()+" sous dans ma poche. Hi ! Hi !");
 		gagnerArgent(butin);
 	}
 	
@@ -22,13 +22,19 @@ public class Yakuza extends Humain{
 	public int perdre() {
 		int sous=getArgent();
 		perdreArgent(sous);
-		parler("J’ai perdu mon duel et mes "+sous+" sous, snif... J'ai déshonoré le clan "+ clan);
+		parler("Jï¿½ai perdu mon duel et mes "+sous+" sous, snif... J'ai dï¿½shonorï¿½ le clan "+ clan);
 		reputation-=1;
 		return sous;			
 	}
 	public void gagner(int gain) {
 		gagnerArgent(gain);
 		reputation+=1;
-		parler("Ce ronin pensait vraiment battre "+getNom()+" du clan de "+clan+"? Je l'ai dépouillé de ses "+gain+" sous.");
+		parler("Ce ronin pensait vraiment battre "+getNom()+" du clan de "+clan+"? Je l'ai dï¿½pouillï¿½ de ses "+gain+" sous.");
+	}
+	
+	@Override
+	public void direbonjour() {
+		super.direbonjour();
+		parler("Mon clan est celui de "+ clan);
 	}
 }
